3562778cfee081c5cd879f4d48636cc11aee1684
jcwitwicki <jc.witwicki@gmail.com>
2017-11-16 21:02:25 +0100

    main method

---
+++ src/main/java/com/jcwitwicki/publicdata/Application.java
@@ -0,0 +1,16 @@
+package com.jcwitwicki.publicdata;
+
+import com.jcwitwicki.publicdata.controller.Prompter;
+
+import java.io.IOException;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+
+        Prompter myPrompter = new Prompter();
+        myPrompter.promptMenu();
+
+    }
+
+}
