1f8c7af478c92a9982ae1b8f083fdbb4fcb7f877
jcwitwicki <jc.witwicki@gmail.com>
2017-11-19 17:52:50 +0100

    implementation of interface CountryDao

---
+++ src/main/java/com/jcwitwicki/publicdata/dao/CountryDaoImpl.java
@@ -0,0 +1,64 @@
+package com.jcwitwicki.publicdata.dao;
+
+import com.jcwitwicki.publicdata.model.Country;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+
+public class CountryDaoImpl implements CountryDao {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public void save(Country country) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(country);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void update(Country country) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.update(country);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(Country country) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(country);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public List<Country> fetchAllCountries() {
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Country> criteria = builder.createQuery(Country.class);
+        criteria.from(Country.class);
+        List<Country> countries = session.createQuery(criteria).getResultList();
+        session.close();
+        return countries;
+    }
+
+    public Country fetchCountryByCode(String countryCode) {
+        Session session = sessionFactory.openSession();
+        Country country = session.get(Country.class,countryCode);
+        session.close();
+        return country;
+    }
+
+}
